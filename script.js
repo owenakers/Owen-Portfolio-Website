90e3e3db1af5bab06881926897abd822e94fcdf1
owen <owenakers10@gmail.com>
2025-03-28 23:41:50 -0400

    reworked entire portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#2563eb',
+                secondary: '#0d9488',
+                dark: '#1e293b',
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                serif: ['Playfair Display', 'serif'],
+            },
+        }
+    }
+}
